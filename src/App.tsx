7b556ca79063ee700c247dd59017d97b8ff1f71b
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2022-12-21 17:43:55 -0300

    Implementação de tipagens do componente Campo

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,21 @@ import { Btn } from "./components/Btn/btn";
 import styles from './App.module.scss';
 
 export default function App() {
+
+
+
   return (
     <div>
       <Btn comBorda={true}>
         Teste botão
       </Btn>
 
-      <Campo className={styles.inserir} placeholder='teste'>teste</Campo>
+      <Campo className={styles.inserir} placeholder='placeholder' type="date" mask="99/99/9999" handleValor={(data: Date) => {
+        console.log(`data: ${data}`)
+      }}>
+        Rótulo
+      </Campo>
+      <Campo className={styles.inserir} placeholder='placeholder'>Rótulo</Campo>
     </div>
   )
 }
--- src/components/Campo/campo.tsx
+++ src/components/Campo/campo.tsx
@@ -1,25 +1,56 @@
-import { HTMLAttributes } from 'react';
+import { HTMLAttributes, useState } from 'react';
 import styles from './Campo.module.scss';
 import classNames from 'classnames';
+import ReactInputMask from 'react-input-mask';
 
 interface Props extends HTMLAttributes<HTMLInputElement> {
-    type?: 'text' | 'email' | 'password' | undefined
+    type?: 'text' | 'email' | 'password' | 'date' | undefined
+    mask?: string
+    handleValor?: Function
 }
 
 export const Campo = (props :Props) => {
-    let type = props.type === undefined ? 'text' : props.type
+    const [valor, setValor] = useState('')
+
+    let handleCampo = (e:any) => {
+        setValor(e.target.value);
+        if (props.type === 'date') {
+            let valorArray = valor.split('/');
+            console.log(valorArray)
+            let data = new Date(Number(valorArray[2]), (Number(valorArray[1]) - 1), Number(valorArray[0]))
+            if (props.handleValor) props.handleValor(data);
+        } else {
+            if (props.handleValor) props.handleValor(valor);
+        }
+    }
+
+    let type;
+    switch(props.type) {
+        case 'date':
+        case undefined:
+            type = 'text';
+            break;
+        default:
+            type = props.type
+    }
 
     return(
-        <label className={classNames({
-            [styles.container]: true,
-            [`${props.className}`]: true
-        })}>
-            <p className={styles.rotulo}>{props.children}</p>
-            <input
-            className={styles.campo}
-            placeholder={props.placeholder}
-            type={type}
-            />
-        </label>
+            <label className={classNames({
+                [styles.container]: true,
+                [`${props.className}`]: true
+            })}>
+                <p className={styles.rotulo}>{props.children}</p>
+                <ReactInputMask
+                mask={props.mask === undefined ? '' : props.mask}
+                placeholder={props.placeholder}
+                type={type}
+                onChange={handleCampo}
+                value={valor}
+                className={classNames({
+                    [styles.campo]: true,
+                    [styles.semValor]: valor === ''
+                })}
+                />
+            </label>
     )
-}   
+}
